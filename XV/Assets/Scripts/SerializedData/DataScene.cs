eaaf561f622a6288dc1a1ed4bc9226003009023e
velocitypulse <velocitypulse@outlook.fr>
2019-04-04 16:38:35 +0200

    [DATASCENE] Add serialization functions

---
+++ XV/Assets/Scripts/SerializedData/DataScene.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[Serializable]
+public sealed class DataScene {
+
+	private const string FILE_NAME = "DataScene.xml";
+
+
+	// When app built, the file is savec in : Build.app/Contents/SavedData/
+
+	static public string Serialize (DataScene iDataScene) {
+		string lFolderPath = Application.dataPath + "/SavedData/";
+		Utils.CreateFolder(lFolderPath);
+
+
+		Debug.Log("Path : " + lFolderPath + FILE_NAME);
+		using (StreamWriter writer = new StreamWriter(lFolderPath + FILE_NAME)) {
+			var serializer = new XmlSerializer(typeof(DataScene));
+			serializer.Serialize(writer, iDataScene);
+			writer.Flush();
+		}
+
+		return lFolderPath + FILE_NAME;
+	}
+
+	static public DataScene Unserialize () {
+
+		string lFolderPath = Application.dataPath + "/SavedData/";
+		Utils.CreateFolder(lFolderPath);
+
+		XmlSerializer lSerializer = new XmlSerializer(typeof(DataScene));
+
+		using (StringReader lStringReader = new StringReader(lFolderPath + FILE_NAME)) {
+			DataScene oDataScene = (DataScene)lSerializer.Deserialize(lStringReader);
+
+			if (oDataScene == null)
+				oDataScene = new DataScene();
+			return oDataScene;
+		}
+	}
+}
---
+++ XV/Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class Utils {
+	
+	public static bool CreateFolder(string iPath) {
+		if (string.IsNullOrEmpty(iPath))
+			return false;
+
+		if (Directory.Exists(iPath))
+			return true;
+		Directory.CreateDirectory(iPath);
+
+		return false;
+	}
+
+}
