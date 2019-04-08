051c5a261319ed45b5b7d439556ca90d45b9f04d
banthony42 <banthony@student.42.fr>
2019-04-08 17:55:29 +0200

    [MODEL VIEWER]	* First version of UI 		* Add sprite for each Model 		* Begin UiModelManager

---
+++ XV/Assets/Scripts/UiElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class UiElement : MonoBehaviour {
+
+
+    [SerializeField]
+    private Color OverlayColor;
+
+
+    public void OnPointerEnter()
+    {
+        GetComponent<Image>().color = OverlayColor;
+    }
+
+    public void OnPointerExit()
+    {
+        GetComponent<Image>().color = Color.white;
+    }
+
+}
---
+++ XV/Assets/Scripts/UiModelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class UiModelManager : MonoBehaviour {
+
+    [Header("UI Element")]
+    [SerializeField]
+    private GameObject UiElement;
+
+    [Header("Sprites Model")]
+    [SerializeField]
+    private Sprite[] UiModelSprites;
+
+
+    [SerializeField]
+    private int MaxModelInView;
+
+    private int mStartModel;
+
+    private List<GameObject> mUiElements;
+
+    private void Start()
+	{
+        if (MaxModelInView <= 0)
+            return;
+        if ((mUiElements = new List<GameObject>(MaxModelInView)) == null)
+            return;
+        mStartModel = 0;
+        for (int i = 0; i < MaxModelInView; i++)
+        {
+            GameObject lTmp = Instantiate(UiElement, transform);
+            lTmp.GetComponent<Image>().sprite = UiModelSprites[i];
+            lTmp.GetComponentInChildren<Text>().text = UiModelSprites[i].name;
+            mUiElements.Add(lTmp);
+        }
+	}
+
+    public void OnScroll()
+    {
+        Debug.Log("COUCOU!");
+    }
+}
