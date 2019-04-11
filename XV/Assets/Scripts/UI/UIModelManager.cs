b86962a63e69cec05b9f317d2c3e7ccb92eb8cd2
banthony42 <banthony@student.42.fr>
2019-04-11 20:23:46 +0200

    [FILE BROWSER]	Create UI prefab [GUI ENTITY]	Update button

--- XV/Assets/Scripts/UI/UIModelManager.cs
+++ XV/Assets/Scripts/UI/UIModelManager.cs
@@ -16,15 +16,13 @@ public sealed class UIModelManager : MonoBehaviour {
         if ((lModels = ModelLoader.Instance.GetAllModel()) == null)
             return;
 
-        GameObject lPaddElement = new GameObject();
-        lPaddElement.name = "PaddingElement";
-        lPaddElement.AddComponent<LayoutElement>();
-
         // Instantiate all UI element to drag and drop model
         // Setting up the sprite of the UI
         // Setting up the text of the UI
         // Store the Model to use at drag and drop,
-        Instantiate(lPaddElement, transform);
+        GameObject lPaddElement = new GameObject("PaddingElement");
+        lPaddElement.transform.parent = transform;
+        lPaddElement.AddComponent<LayoutElement>();
         for (int i = 0; i < lModels.Count; i++) {
             GameObject lUiElement = Instantiate(UiElement, transform);
             lUiElement.GetComponent<Image>().sprite = lModels[i].Sprite;
