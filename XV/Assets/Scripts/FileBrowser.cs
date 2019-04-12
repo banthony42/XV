609e209b84fa6cb6cdce5b8ac8644e11c82a1dc2
banthony42 <banthony@student.42.fr>
2019-04-12 17:08:02 +0200

    [FILE BROWSER] Implement naviguation mechanics - Update visual

--- XV/Assets/Scripts/FileBrowser.cs
+++ XV/Assets/Scripts/FileBrowser.cs
@@ -8,8 +8,14 @@ using UnityEngine.UI;
 
 public class FileBrowser : MonoBehaviour
 {
+    private const string SELECTION_FIELD = "Please select a model file to open ...";
+
     private readonly string STARTING_PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
  
+    private readonly Color32 ROYAL_BLUE = new Color32(65, 105, 255, 255);
+
+    private readonly Color32 ROYAL_GREY = new Color32(80, 80, 80, 255);
+
     private bool mDisplayed;
 
     private CanvasGroup mCanvasGroup;
@@ -23,6 +29,8 @@ public class FileBrowser : MonoBehaviour
     private Text mFileText;
 
     private Image mFileIcon;
+
+    private string mPath;
 
     [Header("Please attach: FileBrowser/Middle/FileContainer/GridWithElements")]
     [SerializeField]
@@ -51,6 +59,7 @@ public class FileBrowser : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
+        mPath = STARTING_PATH;
         mCanvasGroup = GetComponent<CanvasGroup>();
         mCanvasGroup.alpha = 0F;
         mCanvasGroup.blocksRaycasts = false;
@@ -60,6 +69,7 @@ public class FileBrowser : MonoBehaviour
         mSpriteFile = Resources.Load<Sprite>("Sprites/UI/Icons/File");
         mSpriteFolder = Resources.Load<Sprite>("Sprites/UI/Icons/Folder");
 
+        // Get the text component of the Element prefab
         if ((mFileText = mFileEntryTemplate.GetComponentInChildren<Text>()) == null)
             return;
 
@@ -67,17 +77,27 @@ public class FileBrowser : MonoBehaviour
         // Get all image
         if ((lImages = mFileEntryTemplate.GetComponentsInChildren<Image>()) == null)
             return;
-        // Store the children img, ignore the img attach to this gameobject
+
+        // Get the image component of the Element prefab (Button icon)
         foreach (Image lImg in lImages) {
+            // Ignore the component of this gameobject, use the children component.
             if (lImg.gameObject.GetInstanceID() != gameObject.GetInstanceID())
                 mFileIcon = lImg;
         }
+
+        // Button setting
+        GoBack.onClick.AddListener(GoToParentDir);
+        GoHome.onClick.AddListener(GoToHome);
+        OpenFile.onClick.AddListener(OpenSelectedFile);
+
+        // Init selected file text
+        SelectedFile.text = SELECTION_FIELD;
     }
 
     private void UpdateFiles()
     {
-        PWD.text = "PATH:" + STARTING_PATH;
-        List<string> lDirs = new List<string>(Directory.GetFileSystemEntries(STARTING_PATH));
+        PWD.text = "Path:" + mPath;
+        List<string> lDirs = new List<string>(Directory.GetFileSystemEntries(mPath));
 
         foreach (string lFile in lDirs)
         {
@@ -85,14 +105,15 @@ public class FileBrowser : MonoBehaviour
             if ((lAttr & FileAttributes.Hidden) == FileAttributes.Hidden)
                 continue;
             else if ((lAttr & FileAttributes.Directory) == FileAttributes.Directory) {
-                    mFileText.color = Color.blue;
-                    mFileIcon.sprite = mSpriteFolder;
+                mFileText.color = ROYAL_BLUE;
+                mFileIcon.sprite = mSpriteFolder;
             } else {
-                    mFileText.color = Color.black;
+                mFileText.color = ROYAL_GREY;
                     mFileIcon.sprite = mSpriteFile;
             }
-            mFileText.text = lFile;
-            Instantiate(mFileEntryTemplate, UIGridElement.transform);            
+            mFileText.text = lFile.Replace(mPath + "/", "");
+            GameObject lElement = Instantiate(mFileEntryTemplate, UIGridElement.transform);
+            lElement.GetComponent<Button>().onClick.AddListener(() => { ElementSelection(lElement); });
         }
     }
 
@@ -103,13 +124,37 @@ public class FileBrowser : MonoBehaviour
         }
     }
 
+    public void ElementSelection(GameObject iButtonClicked = null)
+    {
+        if (iButtonClicked) {
+            Text lButtonText = iButtonClicked.GetComponentInChildren<Text>();
+            if (lButtonText != null) {
+                FileAttributes lAttr = File.GetAttributes(mPath + "/" + lButtonText.text);
+                if ((lAttr & FileAttributes.Directory) != FileAttributes.Directory)
+                    SelectedFile.text = lButtonText.text;
+                else {
+                    mPath = mPath + "/" + lButtonText.text;
+                    ClearFiles();
+                    UpdateFiles();
+                }
+            }
+        }
+    }
+
     public void GoToParentDir()
     {
+        if (mPath == STARTING_PATH)
+            return;
+        mPath = Directory.GetParent(mPath).FullName;
+        ClearFiles();
+        UpdateFiles();
     }
 
     public void GoToHome()
     {
-        
+        mPath = STARTING_PATH;
+        ClearFiles();
+        UpdateFiles();
     }
 
     public void OpenSelectedFile()
@@ -126,17 +171,17 @@ public class FileBrowser : MonoBehaviour
             mDisplayed = true;
             enabled = true;
             mCanvasGroup.blocksRaycasts = true;
-            StartCoroutine(FadeTo(1F, 0.4F, null));
+            StartCoroutine(FadeToAsync(1F, 0.4F, null));
         } 
         // Hide
         else {
             mDisplayed = false;
             mCanvasGroup.blocksRaycasts = false;
-            StartCoroutine(FadeTo(0F, 0.4F, ClearFiles));
+            StartCoroutine(FadeToAsync(0F, 0.4F, ClearFiles));
         }
     }
 
-    IEnumerator FadeTo(float iValue, float iTime, Action iOnEndFade)
+    IEnumerator FadeToAsync(float iValue, float iTime, Action iOnEndFade)
     {
         float lAlpha = mCanvasGroup.alpha;
 
