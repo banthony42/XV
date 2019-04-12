d434d46a86afa7d7eb6bed240221acea029fd4d7
banthony42 <banthony@student.42.fr>
2019-04-12 15:11:26 +0200

    [FILE BROWSER] Update prefab - List file OK

--- XV/Assets/Scripts/FileBrowser.cs
+++ XV/Assets/Scripts/FileBrowser.cs
@@ -1,15 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FileBrowser : MonoBehaviour
 {
+    private readonly string STARTING_PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+ 
     private bool mDisplayed;
+
     private CanvasGroup mCanvasGroup;
 
-    [Header("Please attach: UIFileBrowserElementGrid")]
+	private GameObject mFileEntryTemplate;
+
+    private Sprite mSpriteFile;
+
+    private Sprite mSpriteFolder;
+
+    private Text mFileText;
+
+    private Image mFileIcon;
+
+    [Header("Please attach: FileBrowser/Middle/FileContainer/GridWithElements")]
     [SerializeField]
-    private GameObject UIFileElement;
+    private GameObject UIGridElement;
+
+    [Header("Please attach: FileBrowser/Top/ButtonContainer/GoBackPath")]
+    [SerializeField]
+    private Button GoBack;
+
+    [Header("Please attach: FileBrowser/Top/ButtonContainer/GoToHome")]
+    [SerializeField]
+    private Button GoHome;
+
+    [Header("Please attach: FileBrowser/Bottom/OpenSelectedFile")]
+    [SerializeField]
+    private Button OpenFile;
+
+    [Header("Please attach: FileBrowser/Top/PWD")]
+    [SerializeField]
+    private Text PWD;
+
+    [Header("Please attach: FileBrowser/Bottom/SelectedFilePath")]
+    [SerializeField]
+    private Text SelectedFile;
 
     // Use this for initialization
     private void Start()
@@ -19,25 +56,87 @@ public class FileBrowser : MonoBehaviour
         mCanvasGroup.blocksRaycasts = false;
         enabled = false;
         mDisplayed = false;
-    }
+        mFileEntryTemplate = Resources.Load<GameObject>("Prefabs/UI/UIFileBrowserElementGrid");
+        mSpriteFile = Resources.Load<Sprite>("Sprites/UI/Icons/File");
+        mSpriteFolder = Resources.Load<Sprite>("Sprites/UI/Icons/Folder");
 
-    public void DisplayToogle()
-    {
-        if (mDisplayed == false) {
-            Debug.Log("BUBBLE : display");
-            mDisplayed = true;
-            enabled = true;
-            mCanvasGroup.blocksRaycasts = true;
-            StartCoroutine(FadeTo(1F, 0.4F));
-        } 
-        else {
-            mDisplayed = false;
-            mCanvasGroup.blocksRaycasts = false;
-            StartCoroutine(FadeTo(0F, 0.4F));            
+        if ((mFileText = mFileEntryTemplate.GetComponentInChildren<Text>()) == null)
+            return;
+
+        Image[] lImages;
+        // Get all image
+        if ((lImages = mFileEntryTemplate.GetComponentsInChildren<Image>()) == null)
+            return;
+        // Store the children img, ignore the img attach to this gameobject
+        foreach (Image lImg in lImages) {
+            if (lImg.gameObject.GetInstanceID() != gameObject.GetInstanceID())
+                mFileIcon = lImg;
         }
     }
 
-    IEnumerator FadeTo(float iValue, float iTime)
+    private void UpdateFiles()
+    {
+        PWD.text = "PATH:" + STARTING_PATH;
+        List<string> dirs = new List<string>(Directory.GetFileSystemEntries(STARTING_PATH));
+
+        foreach (string lFile in dirs)
+        {
+            FileAttributes lAttr = File.GetAttributes(lFile);
+            if ((lAttr & FileAttributes.Hidden) == FileAttributes.Hidden)
+                continue;
+            else if ((lAttr & FileAttributes.Directory) == FileAttributes.Directory) {
+                    mFileText.color = Color.blue;
+                    mFileIcon.sprite = mSpriteFolder;
+            } else {
+                    mFileText.color = Color.black;
+                    mFileIcon.sprite = mSpriteFile;
+            }
+            mFileText.text = lFile;
+            Instantiate(mFileEntryTemplate, UIGridElement.transform);            
+        }
+    }
+
+    private void ClearFiles()
+    {
+        foreach (Transform child in UIGridElement.transform) {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void GoToParentDir()
+    {
+    }
+
+    public void GoToHome()
+    {
+        
+    }
+
+    public void OpenSelectedFile()
+    {
+        
+    }
+
+    // This function toogle the display of the UI
+    public void DisplayToogle()
+    {
+        // Display
+        if (mDisplayed == false) {
+            UpdateFiles();
+            mDisplayed = true;
+            enabled = true;
+            mCanvasGroup.blocksRaycasts = true;
+            StartCoroutine(FadeTo(1F, 0.4F, null));
+        } 
+        // Hide
+        else {
+            mDisplayed = false;
+            mCanvasGroup.blocksRaycasts = false;
+            StartCoroutine(FadeTo(0F, 0.4F, ClearFiles));
+        }
+    }
+
+    IEnumerator FadeTo(float iValue, float iTime, Action iOnEndFade)
     {
         float lAlpha = mCanvasGroup.alpha;
 
@@ -46,5 +145,7 @@ public class FileBrowser : MonoBehaviour
             mCanvasGroup.alpha = newAlpha;
             yield return null;
         }
+        if (iOnEndFade != null)
+            iOnEndFade();
     }
 }
