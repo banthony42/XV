73b50bae4e632d1029389a291e4403fdffc936cd
4quet <louis.fourquet@gmail.com>
2019-04-19 11:27:48 +0200

    [TIMELINE] Basic timeline modifications at runtime: adding/removing tracks

---
+++ XV/Assets/Scripts/Timeline/TimelineManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class TimelineManager : MonoBehaviour {
+
+	public static TimelineManager Instance { get; private set; }
+
+	private PlayableDirector mDirector;
+	private TimelineAsset mTimeline;
+	private List<GameObject> mBoundObjects;
+
+#if UNITY_EDITOR
+	private EditorWindow mTimelineWindow;
+#endif
+
+	private void Start()
+	{
+		if (Instance == null) {
+			Instance = this;
+		}
+		mDirector = GetComponent<PlayableDirector>();
+		mTimeline = (TimelineAsset)mDirector.playableAsset;
+		ClearTimeline();
+	}
+
+	public void AddObject(GameObject iObject)
+	{
+		if (iObject != null) {
+			AnimationTrack lTrack = (AnimationTrack)mTimeline.CreateTrack(typeof(AnimationTrack), null, iObject.name);
+			mDirector.SetGenericBinding(lTrack, iObject);
+		}
+	}
+
+	public void RemoveObject(GameObject iObject)
+	{
+#if UNITY_EDITOR
+		// Removing a track from the timeline at runtime causes errors in the timeline EditorWindow.
+		// This closes the timeline EditorWindow before removing the track to avoid these errors.
+		CloseTimelineWindow();
+#endif
+		if (iObject != null) {
+			TrackAsset lTrackToDelete = GetTrackWithName(iObject.name);
+			if (lTrackToDelete != null) {
+				mTimeline.DeleteTrack(lTrackToDelete);
+			}
+		}
+	}
+
+	private TrackAsset GetTrackWithName(string iTrackName) {
+		foreach (TrackAsset lTrack in mTimeline.GetRootTracks()) {
+			if (lTrack.name == iTrackName) {
+				return lTrack;
+			}
+		}
+		return null;
+	}
+
+	private void ClearTimeline()
+	{
+		List<TrackAsset> lToDelete = new List<TrackAsset>();
+		foreach (TrackAsset lRootTrack in mTimeline.GetRootTracks()) {
+			lToDelete.Add(lRootTrack);
+		}
+		foreach (TrackAsset lRootTrack in lToDelete) {
+			mTimeline.DeleteTrack(lRootTrack);
+		}
+	}
+
+#if UNITY_EDITOR
+	private void CloseTimelineWindow()
+	{
+		if (mTimelineWindow == null) {
+			mTimelineWindow = GetTimelineWindow();
+		}
+		if (mTimelineWindow != null) {
+			mTimelineWindow.Close();
+		}
+	}
+
+	private EditorWindow GetTimelineWindow()
+	{
+		EditorWindow[] lAllWindows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+		foreach (EditorWindow lWin in lAllWindows) {
+			if (lWin.GetType().Name == "TimelineWindow") {
+				return lWin;
+			}
+		}
+		return null;
+	}
+#endif
+}
---
+++ XV/Assets/Scripts/Timeline/TimelineTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTest : MonoBehaviour {
+
+	private int mNum = 0;
+
+	private List<GameObject> mObjects;
+
+	private void Start()
+	{
+		mObjects = new List<GameObject>();
+	}
+
+	public void AddToTimeline() {
+		GameObject lNewObject = new GameObject("Object" + mNum.ToString());
+		lNewObject.AddComponent<Animator>();
+		TimelineManager.Instance.AddObject(lNewObject);
+		mObjects.Add(lNewObject);
+		mNum++;
+	}
+
+	public void DeleteFromTimeline() {
+		if (mObjects.Count > 0) {
+			GameObject lLast = mObjects[mObjects.Count - 1];
+			TimelineManager.Instance.RemoveObject(lLast);
+			mObjects.RemoveAt(mObjects.Count - 1);
+			GameObject.Destroy(lLast);
+		}
+	}
+}
