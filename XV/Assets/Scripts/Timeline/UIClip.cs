03dd15ab6fa51ab3902eec719f19700260493433
4quet <louis.fourquet@gmail.com>
2019-04-19 18:17:44 +0200

    [TIMELINE] UI Clip

---
+++ XV/Assets/Scripts/Timeline/UIClip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIClip : MonoBehaviour, IPointerDownHandler, IDragHandler {
+
+    private RectTransform mTrackRectTransform;
+	private RectTransform mRectTransform;
+
+    private void Start() {
+        mTrackRectTransform = transform.parent as RectTransform;
+		mRectTransform = transform as RectTransform;
+    }
+
+    public void OnPointerDown(PointerEventData iData) {
+        mRectTransform.SetAsLastSibling ();
+    }
+
+    public void OnDrag(PointerEventData iData) {
+        Vector2 lPointerPostion = ClampToTrack(iData);
+        Vector2 lLocalPointerPosition;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle (
+            mTrackRectTransform, lPointerPostion, iData.pressEventCamera, out lLocalPointerPosition
+        )) {
+            mRectTransform.localPosition = lLocalPointerPosition;
+        }
+    }
+
+    Vector2 ClampToTrack(PointerEventData iData) {
+        float lRawPointerPosition = iData.position.x;
+        Vector3[] lTrackCorners = new Vector3[4];
+
+        mTrackRectTransform.GetWorldCorners(lTrackCorners);
+
+		float lMinX = lTrackCorners[0].x + mRectTransform.sizeDelta.x / 2.0F;
+		float lMaxX = lTrackCorners[2].x - mRectTransform.sizeDelta.x / 2.0F;
+        float lClampedX = Mathf.Clamp(lRawPointerPosition, lMinX, lMaxX);
+
+        return new Vector2(lClampedX, mTrackRectTransform.position.y);
+    }
+}
---
+++ XV/Assets/Scripts/Timeline/UIClipResizeHandle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIClipResizeHandle : MonoBehaviour, IPointerDownHandler, IDragHandler {
+
+	private RectTransform mRectTransform;
+
+	public enum HandleSide { HANDLE_LEFT, HANDLE_RIGHT }
+
+	[SerializeField]
+	private HandleSide side;
+
+	private float mSizeMin;
+	private float mSizeMax;
+	private Vector2 mCurrentPointerPos;
+	private Vector2 mPreviousPointerPos;
+
+	private void Start()
+	{
+		mRectTransform = transform.parent.GetComponent<RectTransform>();
+		mSizeMin = 25.0F;
+		mSizeMax = 300.0F;
+	}
+
+	public void OnPointerDown(PointerEventData iData)
+	{
+		mRectTransform.SetAsLastSibling();
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(mRectTransform, iData.position, iData.pressEventCamera, out mPreviousPointerPos);
+	}
+
+	public void OnDrag(PointerEventData iData)
+	{
+		float lSizeDelta = mRectTransform.sizeDelta.x;
+		float lResizeValue = 0F;
+
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(mRectTransform, iData.position, iData.pressEventCamera, out mCurrentPointerPos);
+
+		if (side == HandleSide.HANDLE_LEFT) {
+			lResizeValue = mPreviousPointerPos.x - mCurrentPointerPos.x;
+		}
+		else {
+			lResizeValue = mCurrentPointerPos.x - mPreviousPointerPos.x;
+		}
+
+		lSizeDelta += lResizeValue;
+
+		float lTmpSizeDelta = Mathf.Clamp(lSizeDelta, mSizeMin, mSizeMax);
+		lResizeValue -= (lSizeDelta - lTmpSizeDelta);
+		lSizeDelta = lTmpSizeDelta;
+
+		if (lSizeDelta != mRectTransform.sizeDelta.x) {
+
+			mRectTransform.sizeDelta = new Vector2(lSizeDelta, mRectTransform.sizeDelta.y);
+			if (side == HandleSide.HANDLE_RIGHT) {
+				mRectTransform.anchoredPosition += new Vector2(lResizeValue / 2.0F, 0F);
+			}
+			else {
+				mRectTransform.anchoredPosition -= new Vector2(lResizeValue / 2.0F, 0F);
+			}
+		}
+		mPreviousPointerPos = mCurrentPointerPos;
+	}
+}
