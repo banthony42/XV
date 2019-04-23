054024509359edb1a6c340ace2ed8af792b7155b
4quet <louis.fourquet@gmail.com>
2019-04-23 16:03:21 +0200

    [TIMELINE] UI Timeline main panel with add/delete track buttons

---
+++ XV/Assets/Scripts/Timeline/UITimeline.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimeline : MonoBehaviour {
+
+	private UITrack mUITrackPrefab;
+	private Stack<UITrack> mTracks;
+
+	[SerializeField]
+	private Transform contentPanel;
+
+	private void Start()
+	{
+		mUITrackPrefab = Resources.Load<UITrack>(GameManager.UITemplatePath + "UITrack");
+		mTracks = new Stack<UITrack>();
+	}
+
+	public void NewTrack()
+	{
+		UITrack lNewTrack = Instantiate(mUITrackPrefab, contentPanel);
+		mTracks.Push(lNewTrack);
+	}
+
+	public void DeleteTrack()
+	{
+		Destroy(mTracks.Pop().gameObject);
+	}
+}
