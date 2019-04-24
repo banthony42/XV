11c07ee6aa873b82da83fd5c9d50ec77c52fcdce
4quet <louis.fourquet@gmail.com>
2019-04-24 18:24:53 +0200

    [TIMELINE] UI Timeline controls (play, pause, stop, loop)

---
+++ XV/Assets/Scripts/Timeline/UITimelineControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimelineControls : MonoBehaviour
+{
+	[SerializeField]
+	private Slider slider;
+	private bool mIsPlaying;
+	private bool mLooping;
+
+	private void Start()
+	{
+		mIsPlaying = false;
+		mLooping = false;
+		slider.value = slider.minValue;
+	}
+
+	private void Update()
+	{
+		if (mIsPlaying) {
+			slider.value += Time.deltaTime;
+		}
+		if (slider.value == slider.maxValue && mLooping) {
+			slider.value = slider.minValue;
+		}
+	}
+
+	public void SetIsPlaying(bool iIsPlaying)
+	{
+		mIsPlaying = iIsPlaying;
+	}
+
+	public void SetLooping(bool iLooping)
+	{
+		mLooping = iLooping;
+	}
+
+	public void Reset()
+	{
+		slider.value = slider.minValue;
+		mIsPlaying = false;
+	}
+}
