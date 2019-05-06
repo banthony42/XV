418d0a5a11749041b521d51cc80994529315005b
4quet <louis.fourquet@gmail.com>
2019-05-06 12:16:09 +0200

    [TIMELINE] Custom assets: Actions

---
+++ XV/Assets/Scripts/Timeline/ActionAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActionAsset : PlayableAsset
+{
+   public override Playable CreatePlayable (PlayableGraph iGraph, GameObject iOwner)
+   {
+	   Playable lPlayable = ScriptPlayable<ActionBehaviour>.Create(iGraph);
+	   return lPlayable;
+   }
+
+}
---
+++ XV/Assets/Scripts/Timeline/ActionTrack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[TrackClipType(typeof(ActionAsset))]
+[TrackBindingType(typeof(GameObject))]
+public class ActionTrack : TrackAsset
+{
+
+}
