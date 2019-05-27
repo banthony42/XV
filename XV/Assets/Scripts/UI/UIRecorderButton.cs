2be0026054867e07f3ce020cc04e8a2cd5c6febb
4quet <louis.fourquet@gmail.com>
2019-05-27 14:03:10 +0200

    [UI] Recorder button changing with RecorderStatus

--- XV/Assets/Scripts/UI/UIRecorderButton.cs
+++ XV/Assets/Scripts/UI/UIRecorderButton.cs
@@ -7,23 +7,22 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class UIRecorderButton : MonoBehaviour
 {
-	private bool mRecording;
 	private Image mImage;
+	private Button mButton;
 	private Text mText;
 
 	private Recorder Recorder { get { return GameManager.Instance.Recorder; } }
 
 	private void Awake()
 	{
-		mRecording = false;
 		mImage = GetComponent<Image>();
+		mButton = GetComponent<Button>();
 		mText = GetComponentInChildren<Text>();
 	}
 
 	private void StartRecord()
 	{
 		Recorder.StartRecord();
-		mRecording = true;
 		mText.text = "Stop Recording";
 		mText.color = Color.white;
 		mImage.color = new Color32(188, 0, 0, 100);
@@ -32,20 +31,38 @@ public class UIRecorderButton : MonoBehaviour
 	private void StopRecord()
 	{
 		Recorder.ReleaseRecord();
-		mRecording = false;
+		mButton.interactable = false;
+		StartCoroutine(UpdateComputingRecorderAsync());
+
+	}
+
+	private IEnumerator UpdateComputingRecorderAsync()
+	{
+		int lDots = 0;
+		while (Recorder.RecorderStatus == RecorderStatus.COMPUTING) {
+			mText.text = "Computing";
+			for (int lIndex = 0; lIndex < lDots; lIndex++) {
+				mText.text += ".";
+			}
+			lDots++;
+			if (lDots > 3) {
+				lDots = 0;
+			}
+			yield return new WaitForSeconds(0.2F);
+		}
 		mText.text = "Start Recording";
 		mText.color = Color.black;
 		mImage.color = Color.white;
+		mButton.interactable = true;
 	}
 
 	public void ToggleRecord()
 	{
-		if (mRecording) {
+		if (Recorder.RecorderStatus == RecorderStatus.RECORDING) {
 			StopRecord();
 		}
-		else {
+		else if (Recorder.RecorderStatus == RecorderStatus.AVAILABLE) {
 			StartRecord();
 		}
 	}
-
 }
