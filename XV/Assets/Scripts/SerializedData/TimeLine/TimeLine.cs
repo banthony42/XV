e1efe9ab32d410e467275c3c20ec0ca5e7afefff
velocitypulse <velocitypulse@outlook.fr>
2019-06-18 11:23:44 +0200

    [SERIALIZATION] Move files

---
+++ XV/Assets/Scripts/SerializedData/TimeLine/TimeLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TimeLineSerialized
+{
+
+	public const string DEFAULT_FILE_NAME = "TimeLine.xml";
+
+	public const string RES_PATH = "/Resources/SavedData/Timeline";
+
+	public const string DEBUG_NAME = "debug";
+
+	public TimeLineSerialized()
+	{
+
+	}
+
+
+
+}
